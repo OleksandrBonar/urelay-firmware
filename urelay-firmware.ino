b2941350e8ac2e1e7f680cd92327475417b6b3ec
Oleksandr Bonar <oleksandr.bonar@outlook.com>
2022-06-02 15:48:11 +0300

    Update urelay-firmware.ino

--- urelay-firmware.ino
+++ urelay-firmware.ino
@@ -12,12 +12,13 @@ const int BTN_PIN = 2; // pin connected to button's pin
 const int PIR_PIN = 3; // pin connected to pir's pin
 
 int on_time = 0;
+int on_mqtt = 0;
 
 WiFiClient wifi;
 PubSubClient mqtt(wifi);
-uButton rly(RLY_PIN, OUTPUT, false);
+uButton rly(RLY_PIN, OUTPUT);
 uButton btn(BTN_PIN);
-uButton pir(PIR_PIN, INPUT, false);
+uButton pir(PIR_PIN, INPUT);
 
 void callback(char* t, byte* p, unsigned int l) {
   String topic(t);
@@ -29,9 +30,9 @@ void callback(char* t, byte* p, unsigned int l) {
   if (topic == "urelay/switch1/setvalue") {
     rly.setState(param == "ON" ? rly.getOnValue() : rly.getOffValue());
   } else if (topic == "urelay/switch2/setvalue") {
-    // btn.setState(param == "ON" ? btn.getOnValue() : btn.getOffValue());
+    // switch2_value = param == "ON" ? LOW : HIGH;
   } else if (topic == "urelay/switch3/setvalue") {
-    // btn.setState(param == "ON" ? btn.getOnValue() : btn.getOffValue());
+    // switch3_value = param == "ON" ? HIGH : LOW;
   }
 }
 
@@ -44,78 +45,66 @@ void setup() {
   btn.begin();
   pir.begin();
 
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("connecting to ");
-  Serial.println(ssid);
+  btn.setDebounceTime(50);
+  pir.setDebounceTime(50);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
-
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("wifi connected");
-  Serial.println("ip address: ");
-  Serial.println(WiFi.localIP());
   
   mqtt.setServer("192.168.0.111", 1883);
   mqtt.setCallback(callback);
 
-  btn.setDebounceTime(50);
-  pir.setDebounceTime(50);
-
   on_time = millis();
+  on_mqtt = millis();
 }
 
 void loop() {
-  if (!mqtt.connected()) {
-    // Loop until we're reconnected
-    while (!mqtt.connected()) {
-      // Attempt to connect
-      if (mqtt.connect("urelay", "mosquitto", "password")) {
-        Serial.println("mqtt connected");
-        // Once connected, publish an announcement...
-        mqtt.publish("urelay/system/getonline", "online");
-        mqtt.publish("urelay/switch1/getvalue", rly.isOn() ? "ON" : "OFF");
-        mqtt.publish("urelay/switch2/getvalue", btn.isOn() ? "ON" : "OFF");
-        mqtt.publish("urelay/switch3/getvalue", pir.isOn() ? "ON" : "OFF");
-        // ... and resubscribe
-        mqtt.subscribe("urelay/switch1/setvalue");
-        mqtt.subscribe("urelay/switch2/setvalue");
-        mqtt.subscribe("urelay/switch3/setvalue");
-      } else {
-        Serial.print("mqtt connection failed, rc=");
-        Serial.print(mqtt.state());
-        Serial.println(" try again in 2 seconds");
-        // Wait 2 seconds before retrying
-        delay(2000);
-      }
+  if (mqtt.connected())  {
+    mqtt.loop();
+  } else if (millis() - on_mqtt >= 2000) {
+    on_mqtt = millis();
+
+    // Attempt to connect
+    if (mqtt.connect("urelay", "mosquitto", "password")) {
+      // Once connected, publish an announcement...
+      mqtt.publish("urelay/system/getonline", "online");
+      mqtt.publish("urelay/switch1/getvalue", rly.isOn() ? "ON" : "OFF");
+      mqtt.publish("urelay/switch2/getvalue", btn.isOn() ? "ON" : "OFF");
+      mqtt.publish("urelay/switch3/getvalue", pir.isOn() ? "ON" : "OFF");
+      // ... and resubscribe
+      mqtt.subscribe("urelay/switch1/setvalue");
+      mqtt.subscribe("urelay/switch2/setvalue");
+      mqtt.subscribe("urelay/switch3/setvalue");
     }
   }
 
-  mqtt.loop();
   rly.loop();
   pir.loop();
   btn.loop();
 
   if (pir.isChanged()) {
-    mqtt.publish("urelay/switch3/getvalue", pir.isOn() ? "ON" : "OFF");
+    if (mqtt.connected()) {
+      mqtt.publish("urelay/switch3/getvalue", pir.isOn() ? "ON" : "OFF");
+    }
+
     Serial.println(pir.isOn() ? "pir is ON" : "pir is OFF");
   }
 
   if (btn.isChanged()) {
     rly.setState(rly.isOn() ? rly.getOffValue() : rly.getOnValue());
 
-    mqtt.publish("urelay/switch2/getvalue", btn.isOn() ? "ON" : "OFF");
+    if (mqtt.connected()) {
+      mqtt.publish("urelay/switch2/getvalue", btn.isOn() ? "ON" : "OFF");
+    }
+
     Serial.println(btn.isOn() ? "btn is ON" : "btn is OFF");
   }
 
   if (rly.isChanged()) {
-    mqtt.publish("urelay/switch1/getvalue", rly.isOn() ? "ON" : "OFF");
+    if (mqtt.connected()) {
+      mqtt.publish("urelay/switch1/getvalue", rly.isOn() ? "ON" : "OFF");
+    }
+
     Serial.println(rly.isOn() ? "rly is ON" : "rly is OFF");
   }
 
@@ -123,10 +112,12 @@ void loop() {
   if (millis() - on_time >= 60000) {
     on_time = millis();
 
-    mqtt.publish("urelay/system/getonline", "online");
-    mqtt.publish("urelay/switch1/getvalue", rly.isOn() ? "ON" : "OFF");
-    mqtt.publish("urelay/switch2/getvalue", btn.isOn() ? "ON" : "OFF");
-    mqtt.publish("urelay/switch3/getvalue", pir.isOn() ? "ON" : "OFF");
+    if (mqtt.connected()) {
+      mqtt.publish("urelay/system/getonline", "online");
+      mqtt.publish("urelay/switch1/getvalue", rly.isOn() ? "ON" : "OFF");
+      mqtt.publish("urelay/switch2/getvalue", btn.isOn() ? "ON" : "OFF");
+      mqtt.publish("urelay/switch3/getvalue", pir.isOn() ? "ON" : "OFF");
+    }
 
     Serial.println("status updated");
   }
